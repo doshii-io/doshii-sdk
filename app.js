c58ec5e241e9feed9b8e41c675be2943e0cbedf6
Jimmy Cann <mail@jimmycann.com>
2016-12-01 07:42:32 +1100

    Ability to factor in additional required header params, updated checkins

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ require('./.config/.env')
 const Doshii = require('./lib/doshii.js')()
 
 Doshii.getApiField('auth')
-Doshii.Locations.retrieveAll().then((result) => {
-  console.log(result)
-}).catch((err) => {
-  console.log(err)
-})
+// Doshii.Locations.retrieveAll().then((result) => {
+//   console.log(result)
+// }).catch((err) => {
+//   console.log(err)
+// })
 
-Doshii.Locations.findMenu({
-  id: 'p7PEkVxe'
+Doshii.Checkins.retrieve({
+  id: 1,
+  doshiiLocationId: 'p7PEkVxe'
 }, (err, result) => {
   if (err) console.log(err)
   console.log(result)

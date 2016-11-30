394465415bb5ab63b79d535caa6836d07c1a6a9b
Jimmy Cann <mail@jimmycann.com>
2016-12-01 09:05:49 +1100

    Added WebSocket support

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ require('./.config/.env')
 
 const Doshii = require('./lib/doshii.js')()
 
-Doshii.getApiField('auth')
-// Doshii.Locations.retrieveAll().then((result) => {
-//   console.log(result)
-// }).catch((err) => {
-//   console.log(err)
-// })
+Doshii.ws.on('message', (event) => {
+  if (!event) return
+  event = JSON.parse(event.data)
+  if (!event.emit) return
+  console.log(event.emit)
+})
+Doshii.ws.on('member_created', (event) => {
+  console.log(event)
+  if (!event) return
+  event = JSON.parse(event.data)
+  if (!event.emit) return
+  console.log(event.emit)
+})
+Doshii.Locations.retrieveAll().then((result) => {
+  console.log(result)
+}).catch((err) => {
+  console.log(err)
+})
 
 Doshii.Checkins.retrieve({
   id: 1,

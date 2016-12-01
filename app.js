e1046718d320c8677bdefe8a2e3b33d384715abc
Jimmy Cann <mail@jimmycann.com>
2016-12-01 22:32:05 +1100

    Add Members methods

--- app.js
+++ app.js
@@ -10,23 +10,55 @@ Doshii.ws.on('message', (event) => {
   if (!event.emit) return
   console.log(event.emit)
 })
+
 Doshii.ws.on('member_created', (event) => {
-  console.log(event)
   if (!event) return
   event = JSON.parse(event.data)
+  console.log(event)
   if (!event.emit) return
   console.log(event.emit)
 })
 
-Doshii.Locations.retrieveAll().then((result) => {
-  console.log(result)
-}).catch((err) => {
-  console.log(err)
-})
+// Doshii.Locations.retrieveAll().then((result) => {
+//   console.log(result)
+// }).catch((err) => {
+//   console.log(err)
+// })
 
-Doshii.Checkins.retrieve({
+// Doshii.Locations.retrieveMenu({
+//   id: 'p7PEkVxe'
+// }).then((result) => {
+//   console.log(result)
+// }).catch((err) => {
+//   console.log(err)
+// })
+
+// Doshii.Checkins.retrieveOne({
+//   id: 1,
+//   doshiiLocationId: 'p7PEkVxe'
+// }, (err, result) => {
+//   if (err) console.log(err)
+//   console.log(result)
+// })
+
+// Doshii.Members.retrieveAll({
+//   doshiiOrganisationId: 2
+// }, (err, result) => {
+//   if (err) console.log(err)
+//   console.log(result)
+// })
+
+// Doshii.Members.retrieveOne({
+//   id: 1,
+//   doshiiOrganisationId: 2
+// }, (err, result) => {
+//   if (err) console.log(err)
+//   console.log(result)
+// })
+
+Doshii.Members.create({
   id: 1,
-  doshiiLocationId: 'p7PEkVxe'
+  doshiiOrganisationId: 2
 }, (err, result) => {
   if (err) console.log(err)
   console.log(result)
